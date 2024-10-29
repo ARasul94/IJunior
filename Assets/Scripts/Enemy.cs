15b37f47909d0b4e1c7b93a2d497310460101d9d
r.aliev <r.aliev@greengreystudio.com>
2024-10-29 19:03:42 +0100

    fix name

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _destoyDelay = 5f;
+    [SerializeField] private float _destroyDelay = 5f;
     [SerializeField] private float _speed = 5f;
     
     private Vector3 _movementDirection;
 
     private void Awake()
     {
-        Destroy(gameObject, _destoyDelay);
+        Destroy(gameObject, _destroyDelay);
+    }
+    
+    private void Update()
+    {
+        transform.position += _movementDirection * _speed * Time.deltaTime;
     }
 
     public void SetMovementDirection(Vector3 direction)
     {
         _movementDirection = direction.normalized;
-    }
-
-    private void Update()
-    {
-        transform.position += _movementDirection * _speed * Time.deltaTime;
     }
 }
