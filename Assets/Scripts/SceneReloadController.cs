e539b7007ce6c0a4d82b59f9074bbd328a82c942
Aliev Rasul <arasul110294@gmail.com>
2024-11-11 21:40:48 +0100

    setup level

---
+++ Assets/Scripts/SceneReloadController.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class SceneReloadController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            Scene activeScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(activeScene.buildIndex);
+        }
+    }
+}
