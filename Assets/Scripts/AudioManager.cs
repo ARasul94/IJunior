51d7ec3444ed0f0530521c45cc5db24abfc27d21
Aliev Rasul <arasul110294@gmail.com>
2025-01-13 13:24:05 +0100

    added solution

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    private const string MusicVolume = "MusicVolume";
+    private const string SoundVolume = "SoundVolume";
+    private const string MasterVolume = "MasterVolume";
+    private const int AudioLevelMultiplayer = 20;
+    private const float AudioMinLevel = 0.0001f;
+    
+    [SerializeField] private Toggle _audioToggler;
+    [SerializeField] private Slider _soundSlider;
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private Slider _masterSlider;
+    [SerializeField] private AudioMixerGroup _audioMixerGroup;
+
+    private bool _isAudioOn = true;
+
+    private void Awake()
+    {
+        _soundSlider.minValue = AudioMinLevel;
+        _musicSlider.minValue = AudioMinLevel;
+        _masterSlider.minValue = AudioMinLevel;
+        
+        _audioToggler.onValueChanged.AddListener(OnAudioStateChanged);
+        _soundSlider.onValueChanged.AddListener(OnSoundVolumeChanged);
+        _musicSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+        _masterSlider.onValueChanged.AddListener(OnMasterVolumeChanged);
+    }
+
+    private void OnAudioStateChanged(bool isOn)
+    {
+        _isAudioOn = isOn;
+        OnMusicVolumeChanged(_musicSlider.value);
+        OnSoundVolumeChanged(_soundSlider.value);
+        OnMasterVolumeChanged(_masterSlider.value);
+    }
+
+    private void OnMusicVolumeChanged(float volume)
+    {
+        SetVolume(MusicVolume, volume);
+    }
+    
+    private void OnSoundVolumeChanged(float volume)
+    {
+        SetVolume(SoundVolume, volume);
+    }
+    
+    private void OnMasterVolumeChanged(float volume)
+    {
+        SetVolume(MasterVolume, volume);
+    }
+
+    private void SetVolume(string mixerName, float volume)
+    {
+        _audioMixerGroup.audioMixer.SetFloat(mixerName, GetAdjustedVolume(volume));
+    }
+
+    private float GetAdjustedVolume(float volume)
+    {
+        return Mathf.Log10(_isAudioOn ? volume : AudioMinLevel) * AudioLevelMultiplayer;
+    }
+}
---
+++ Assets/Scripts/SoundButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button),typeof(AudioSource))]
+public class SoundButton : MonoBehaviour
+{
+    private Button _button;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _audioSource = GetComponent<AudioSource>();
+        
+        _button.onClick.AddListener(OnButtonClicked);
+    }
+
+    private void OnButtonClicked()
+    {
+        _audioSource.Play();
+    }
+}
