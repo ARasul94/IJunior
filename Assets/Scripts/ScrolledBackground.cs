48cc358b86b33e08bf065e1c9158176cda9f6b91
Aliev Rasul <arasul110294@gmail.com>
2025-03-24 14:06:49 +0100

    added sources

---
+++ Assets/Scripts/ScrolledBackground.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+public class ScrolledBackground : MonoBehaviour
+{
+    [SerializeField] private float _scrollYSpeed = 0.1f;
+    [SerializeField] private float _scrollXSpeed = 0.5f;
+
+    private MeshRenderer _meshRenderer;
+    private Material _material;
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _material = _meshRenderer.material;
+    }
+
+    private void Update()
+    {
+        var xOffset = Input.GetAxis("Horizontal") * _scrollXSpeed;
+        var verticalInput = Input.GetAxis("Vertical");
+        var yOffset = _scrollYSpeed + (verticalInput > 0 ? verticalInput : 0) * _scrollYSpeed;
+        var offset = new Vector2(xOffset, yOffset);
+        _material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
